10b8092e40508be3534a539dfd0b49db896e6525
abdrafay <zainrafay12@gmail.com>
2022-11-05 15:04:49 +0500

    Some Other Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+// React Functionns
+import { useEffect, useReducer } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Axios from "axios";
+
+// Cookie utils
+import { getCookie, setCookie, erase } from "./utils/cookies";
+import Home from './Screens/Home';
+
 
 function App() {
+  const initialState = {
+    apiEndPoint: "https://nomadrof.herokuapp.com",
+    loggedIn: (getCookie("token") !== 'undefined' && getCookie('token') ) ? true : false,
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
+    token: getCookie("token") ? getCookie("token") : null,
+    roles: localStorage.getItem("roles") ? JSON.parse(localStorage.getItem("roles")) : [],
+  };
+  const ourReducer = (state, action) => {
+    switch (action.type) {
+      case "LOGIN":
+        return {
+          ...state,
+          loggedIn: true,
+          user: action.payload,
+          token: action.token,
+        };
+        case "UPDATE_ROLES":
+          return {
+            ...state,
+            roles: action.payload,
+          };
+      case "LOGOUT":
+        return {
+          ...state,
+          loggedIn: false,
+          user: {},
+          token: "",
+        };
+      default:
+        return state;
+    }
+  };
+  const [state, dispatch] = useReducer(ourReducer, initialState);
+  
+  useEffect(()=> {
+    
+    const getRoles = async () => {
+      try {
+        const response = await Axios.get(`${state.apiEndPoint}/api/roles`);
+        if (response.status === 200) {
+          dispatch({type: "UPDATE_ROLES", payload: response.data.roles});
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getRoles();
+  },[])
+  useEffect(()  => {
+    if(state.loggedIn) {
+      localStorage.setItem("user", JSON.stringify(state.user));
+      setCookie("token", state.token, 7);
+      return;
+    }
+    localStorage.removeItem("user");
+    erase("token");
+  },[state.loggedIn, state.user,state.token])
+
+  useEffect(() => {
+    if(state.roles) {
+      localStorage.setItem("roles", JSON.stringify(state.roles));
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
