021a6c6e5ccb73c306aed9822ba13884e76aff3f
abdrafay <zainrafay12@gmail.com>
2022-11-05 16:14:50 +0500

    DEsgin changes

--- src/Components/SMButton.jsx
+++ src/Components/SMButton.jsx
@@ -1,16 +1,17 @@
-import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
+// import Button from '@mui/material/Button';
+// import CircularProgress from '@mui/material/CircularProgress';
 
 
 const SMbuttons = ({label, onClick, loading, disabled, id, type}) => {
 
     return (
-        type=="submit" ? 
-        <Button id={id} type={type} disabled={disabled || loading}
-            variant="outlined">{loading ? <CircularProgress /> : label}</Button>
-        :
-        <Button id={id} onClick={onClick} disabled={disabled || loading}
-            variant="outlined">{loading ? <CircularProgress /> : label}</Button>
+        <button>A button</button>
+        // type=="submit" ? 
+        // <Button id={id} type={type} disabled={disabled || loading}
+        //     variant="outlined">{loading ? <CircularProgress /> : label}</Button>
+        // :
+        // <Button id={id} onClick={onClick} disabled={disabled || loading}
+        //     variant="outlined">{loading ? <CircularProgress /> : label}</Button>
         
     )
 }
