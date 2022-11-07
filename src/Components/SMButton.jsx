c56fb92005893abe44f6cdbe2a3df682c41cf767
abdrafay <zainrafay12@gmail.com>
2022-11-08 02:20:06 +0500

    Checking for errors

--- src/Components/SMButton.jsx
+++ src/Components/SMButton.jsx
@@ -2,14 +2,14 @@ import Button from '@mui/material/Button';
 // import CircularProgress from '@mui/material/CircularProgress';
 
 
-const SMbuttons = ({label, onClick, loading, disabled, id, type}) => {
+const SMbuttons = ({label, id, type}) => {
 
     return (
-        // <button>A button</button>
-        type ? 
-        <Button id={id} type={type}
-            variant="outlined">{label}</Button> : <Button id={id}
-            variant="outlined">{label}</Button>  
+        <button>A button</button>
+        // type ? 
+        // <Button id={id} type={type}
+        //     variant="outlined">{label}</Button> : <Button id={id}
+        //     variant="outlined">{label}</Button>  
     )
 }
 
