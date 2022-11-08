76a6f4ec8868641bee0883e24d553d6e5fa41a6d
abdrafay <zainrafay12@gmail.com>
2022-11-09 02:58:26 +0500

    Origin Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useReducer } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Axios from "axios";
 
-// import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 // Cookie utils
 import { getCookie, setCookie, erase } from "./utils/cookies";
 import Home from './Screens/Home';
@@ -81,7 +81,7 @@ function App() {
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
     <BrowserRouter>
-    {/* <AccessTimeFilledIcon /> */}
+    <AccessTimeFilledIcon />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />        
